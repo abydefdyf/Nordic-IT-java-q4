780678c73777afe47a89dd71d75c9b5013a277a4
abydefdyf <abydefdyf@gmail.com>
2019-12-02 16:19:21 +0300

    Урок 13

---
+++ Lession13/com/example/CashboxUpdate.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class CashboxUpdate {
+
+  public static void main(String[] args) throws InterruptedException {
+    var visitors = new LinkedBlockingDeque<Integer>();
+    var vector = new LinkedBlockingDeque<Integer>();
+
+
+    for (var i = 1; i < 2001; i++) {
+      visitors.add(i);
+
+    }
+
+    System.out.println("В магазин пришло : " + visitors.size() + " посетителей");
+    System.out.println("Посетителей в очереди: " + visitors);
+    var executorService = Executors.newFixedThreadPool(4);
+
+    executorService.invokeAll(
+        Arrays.asList(
+            Executors.callable(new nextGenerationUpdate(visitors, vector)),
+            Executors.callable(new nextGenerationUpdate(visitors, vector)),
+            Executors.callable(new nextGenerationUpdate(visitors, vector)),
+            Executors.callable(new nextGenerationUpdate(visitors, vector))
+        )
+    );
+    executorService.shutdown();
+
+
+    System.out.println("Обрабатывем посетителей: " + vector);
+    System.out.println("Всего обработано: " + vector.size() + " посетителей");
+
+  }
+
+}
+
+
---
+++ Lession13/com/example/nextGenerationUpdate.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class nextGenerationUpdate implements Runnable {
+  private LinkedBlockingDeque<Integer> visitors;
+  private LinkedBlockingDeque<Integer> vector;
+
+
+  public nextGenerationUpdate(LinkedBlockingDeque<Integer> visitors,
+      LinkedBlockingDeque<Integer> vector) {
+    super();
+    this.visitors = visitors;
+    this.vector = vector;
+  }
+
+
+  @Override
+  public void run() {
+    while (moveElement()) {
+      if (Thread.interrupted()) {
+        
+      }
+    }
+  }
+
+
+  private boolean moveElement() {
+    var element = visitors.pollFirst();
+    
+    if (element != null) {
+      vector.add(element);
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+}
